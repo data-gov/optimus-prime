9bb0f8e5a68aa483e8a12a83ec3a0b336338b3af
Eduardo Moroni <eduardomoroni@gmail.com>
2017-10-18 21:10:54 -0200

    feat: adiciona logger para o mongo

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,9 +1,20 @@
-import { MongoClient } from 'mongodb'
+import { Logger, MongoClient } from 'mongodb'
 
 export const mongoConnection = async () => {
   const db = await MongoClient.connect(process.env.MONGO_URL)
+  setupLogger()
   return {
     Links: db.collection('links'),
     Expenses: db.collection('expenses')
   }
+}
+
+const setupLogger = () => {
+  let logCount = 0
+  Logger.setCurrentLogger(msg => {
+    console.log(`MONGO DB REQUEST ${++logCount}:`)
+    console.log(msg)
+  })
+  Logger.setLevel('debug')
+  Logger.filter('class', ['Cursor'])
 }
