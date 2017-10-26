30831419024f36a02d88a25047d52bb4e86a785b
Eduardo Moroni <eduardomoroni@gmail.com>
2017-10-26 18:12:08 -0200

    feat: use apollo-engine only in production

--- src/app.js
+++ src/app.js
@@ -4,16 +4,15 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import compression from 'compression'
 
-import { initGraphEngine } from './config/graphqlEngine'
+import { startApolloEngine } from './config/apolloEngine'
 import { initConfigurations } from './config'
 import { router } from './routes'
 
 export const initializeApp = async (port) => {
   await initConfigurations()
-  const engine = await initGraphEngine()
   const app = express()
+  await startApolloEngine(app)
 
-  app.use(engine.expressMiddleware())
   app.use(bodyParser.urlencoded({ extended: false }))
   app.disable('x-powered-by')
   app.use(bodyParser.json())
