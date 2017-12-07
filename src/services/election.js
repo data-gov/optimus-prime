2273c6db0e0ca3ffedf1363136265768437489d9
Eduardo Moroni <eduardomoroni@gmail.com>
2017-12-07 08:59:20 -0200

    refactor: remove mocked data service

--- src/services/election.js
+++ src/services/election.js
@@ -1,12 +1,4 @@
-import mockedElectionData from './mockedElectionData'
-import {findCongressmanById} from './congressman'
 import {byRoleAndYear} from '../clients/mongo/election'
-
-export const findMostVotedDeputyByState = async (state) => {
-  const votes = mockedElectionData[state]
-  const mostVoted = votes.sort((a, b) => b.votes - a.votes)[0]
-  return findCongressmanById(mostVoted.id)
-}
 
 export const findCandidatesByRoleAndYear = async (role, year) => {
   const election = await byRoleAndYear(role, year)
@@ -15,6 +7,5 @@ export const findCandidatesByRoleAndYear = async (role, year) => {
 }
 
 export default {
-  findMostVotedDeputyByState,
   findCandidatesByRoleAndYear
 }
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,7 @@
 import CongressmenService from './congressman'
 import ElectionService from './election'
-import MockedDataService from './mockedData'
 
 export const services = {
   CongressmenService,
-  ElectionService,
-  MockedDataService
+  ElectionService
 }
